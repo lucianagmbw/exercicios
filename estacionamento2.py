ed3bc1cc9c5bb7e1a8bee44c5ced576477fac517
unknown <lucianagmbw@gmail.com>
2022-08-09 02:56:29 -0300

    mais exercicios

--- estacionamento2.py
+++ estacionamento2.py
@@ -1,102 +1,135 @@
 
-class Estacionamento:
-    def __init__(self) -> None:
-       
-        self.vagas_de_carro = 5
-        self.vagas_de_moto = 5
-        self.carro_para_vaga =1
-        self.moto_para_vaga =1
-        self.total_vagas_livres_carro = 5
-        self.total_vagas_livres_moto = 5
+class Carro:
+    def __init__(self, placa) -> None:
 
-    def estacionar_carro(self):
-        
-        return 
+        self.placa = placa
+        self.estacionado = False
 
-    def estacionar_moto(self):
+    def estacionar(self, Vaga):
+        if self.estacionado:
+            return
+        self.estacionado = True
 
-        return
+    def sair_da_vaga(self, Vaga):
+        if self.estacionado:
+            self.estacionado = False
 
-    def remover_carro(self):
 
-        return
+class Moto:
+    def __init__(self, placa) -> None:
 
-    def remover_moto(self):
+        self.placa = placa
+        self.estacionado = False
 
-        return  
-
-    def estado_do_estacionamento(self):
-
-        return
-
-    class Vaga:
-        def __init__(self,placa,tipo) -> None:
-            self.id = 1 
-            self.tipo = tipo #vaga-de_carro ou vaga_de_moto
-            self.livre = True
-            self.placa = placa
-
-        def ocupar(self):
-            self.id = self.id
-            self.id += self.id
-
+    def estacionar(self, Vaga):
+        if self.estacionado:
             return
 
-        def desocupar():
-            
-            return    
+        self.estacionado = True
 
-
-    class Carro:
-        def __init__(self,placa) -> None:
-
-            self.placa = placa
+    def sair_da_vaga(self, Vaga):
+        if self.estacionado:
             self.estacionado = False
 
-        def estacionar(self,Vaga):
-            if self.estacionado:
-                return
             
-            self.estacionado = True
 
 
-        def sair_da_vaga(self,Vaga):
-            if self.estacionado:
-                self.estacionado = False
-                
-                return                           
+class Vaga:
+    def __init__(self, id, tipo) -> None:
+        self.id = id
+        self.tipo = tipo  # vaga-de_carro ou vaga_de_moto
+        self.livre = True
+        self.placa = None
+
+    def ocupar(self, placa):
+        self.placa = placa
+        self.livre = False
+
+    def desocupar(self, placa):
+        self.placa = placa
+        self.livre = True
 
 
-    
-    class Moto:
-        def __init__(self,placa) -> None:
+class Estacionamento:
+    def __init__(self) -> None:
 
-            self.placa = placa
-            self.estacionado = False
+        self.vagas_de_carro = []
+        self.vagas_de_moto = []
+        self.carro_para_vaga = 1
+        self.moto_para_vaga = 1
+        self.total_vagas_livres_carro = 5
+        self.total_vagas_livres_moto = 5
+        self.exibir_vagas()
+
+    def exibir_vagas(self):
+        tipo = 'carro'
         
-        def estacionar(self,Vaga):
-            if self.estacionado:
-                return
-            
-            self.estacionado = True
+        for i in range(self.total_vagas_livres_carro):
+            self.vagas_de_carro[i] =  Vaga(i, i, tipo)
+
+        tipo = 'moto'
+        for j in range(self.total_vagas_livres_moto):
+            self.vagas_de_moto[j] = Vaga(j, j, tipo)
+
+    def estacionar_carro(self, carro: Carro):
+        if carro.estacionado is False:
+            if self.total_vagas_livres_carro > 0:
+
+                vaga = self.vagas_de_carro[self.total_vagas_livres_carro]
+                vaga.ocupar(carro.placa)
+
+                carro.estacionar(vaga)
+                self.total_vagas_livres_carro -= 1
+
+    def estacionar_moto(self, moto: Moto):
+        if moto.estacionado is False:
+            if self.total_vagas_livres_carro < 0:
+
+                vaga = self.vagas_de_carro[self.total_vagas_livres_carro]
+                vaga.ocupar(moto.placa)
+
+                moto.estacionar(vaga)
+                self.total_vagas_livres_moto -= 1
+
+    def remover_carro(self, carro:Carro):
+        if carro.estacionado is True:
+               
+            vaga = self.vagas_de_carro[self.total_vagas_livres_carro]
+            vaga.desocupar(carro.placa)
+            carro.sair_da_vaga(vaga)
+            self.total_vagas_livres_carro += 1
+
+    def remover_moto(self , moto:Moto):
+        if moto.estacionado is True:
+               
+            vaga = self.vagas_de_moto[self.total_vagas_livres_moto]
+            vaga.desocupar(moto.placa)
+            moto.sair_da_vaga(vaga)
+            self.total_vagas_livres_moto += 1
+        
+
+    def estado_do_estacionamento(self):
+        num_carros_estacionados = 5 - self.total_vagas_livres_carro 
+        num_motos_estacionadas = 5 -  self.total_vagas_livres_moto
+
+        estado += f'  Total de vagas livres de carros: {self.total_vagas_livres_carro}\n'
+        estado += f'  Total de vagas livres de motos: {self.total_vagas_livres_moto}\n'
+        estado += f'  Num carros estacionados: {num_carros_estacionados}\n'
+        estado += f'  Num motos estacionadas: {num_motos_estacionadas}\n'
+
+        for i in range(len(self.vagas_carro)):
+            placa = self.vagas_carro[i].placa
+            estado += f'vaga[{i}]: {placa}; '
+        estado += f'\n  Vagas de moto:\n    '
 
 
-        def sair_da_vaga(self,Vaga):
-            if self.estacionado:
-                self.estacionado = False
-                
-                return        
-                                
-            
+
+    def __str__(self):
+        return self.estado_do_estacionamento()
+
+carro = Carro('LHL3256' )
 
 
 
-    carro = Carro ('LHL3256' )
-    carro.estacionar()
-    carro.sair_da_vaga()    
 
-
-
-    moto = Moto ('MZM')
-    moto.estacionar()
-    moto.sair_da_vaga()
+moto = Moto('MZM' )
